ea9ab3fba5e0803d9613840057fbad14d34d34be
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 09:34:18 +0000

    add input prompts for user name and display the entered name

--- 1_3_basic_io.c
+++ 1_3_basic_io.c
@@ -14,5 +14,10 @@ int main()
     printf("Enter the value of z: ");
     scanf("%d", &z); // address of operation & it holds address where the value from scanf should reside.
     printf("Value of Z is %d\n", z);
+
+    char name[20]; // char array of 20 characters. Ends with '\0' to represent end of string.
+    printf("Enter your name: ");
+    scanf("%19s", name); // no need to add & as arrays decode to pointers
+    printf("Your name is %s\n", name);
     return 0;
 }
