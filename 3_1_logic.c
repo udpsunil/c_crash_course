3d37f98e7b0e997b47aed8023e631717c26f1095
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 14:16:32 +0000

    add switch statement to handle menu choices

--- 3_1_logic.c
+++ 3_1_logic.c
@@ -6,6 +6,7 @@ int main()
     int age = 17;
     double money = 30000;
     bool is_graduated = true;
+    int menu_choice = 0;
 
     if (age > 17 && money > 25000 || is_graduated)
     {
@@ -14,6 +15,29 @@ int main()
     else
     {
         printf("This is not an adult\n");
+    }
+
+    switch (menu_choice)
+    {
+    case 0:
+        /* code */
+        printf("Case 0\n");
+        break;
+    case 1:
+        printf("Case 1\n");
+        break;
+
+    case 2:
+        printf("Case 2\n");
+        break;
+
+    case 3:
+        printf("Case 3\n");
+        break;
+
+    default:
+        printf("Default case\n");
+        break;
     }
     return 0;
 }
