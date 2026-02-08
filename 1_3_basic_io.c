b377ce8559c0f6d17bc05030580f969608b738d7
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 09:11:39 +0000

    Variable declaration and definition

--- 1_3_basic_io.c
+++ 1_3_basic_io.c
@@ -3,5 +3,10 @@
 int main()
 {
     printf("Hello World\n");
+
+    int x = 50; // identifier x is used to represent value of 50
+    int y; //declaration of variable y with a type
+    y = 10; //initialization done with assignment operation.
+    
     return 0;
 }
