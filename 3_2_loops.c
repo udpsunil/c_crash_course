534158befa7a399bc8704ad870bd029ae99ccd17
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 14:35:53 +0000

    refactor loop implementation to display ages and reverse counting

--- 3_2_loops.c
+++ 3_2_loops.c
@@ -2,9 +2,22 @@
 
 int main()
 {
-    for (int i = 0; i < 100; i++)
+    int size = 10;
+    int ages[] = {12, 43, 54, 3, 5, 53, 6, 7, 87, 122};
+    int calculated_size = sizeof(ages) / sizeof(ages[0]);
+
+    for (int i = 0; i < calculated_size; i++)
     {
-        printf("%d\n", i);
+        printf("Ages[%d] = %d\n", i, ages[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
     }
     return 0;
 }
