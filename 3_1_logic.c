1a2089394fc59596a0be25a62fb3f0c641d640c1
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 11:43:39 +0000

    add logic to determine adult status based on age, money, and graduation status

--- 3_1_logic.c
+++ 3_1_logic.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    
+    int age = 17;
+    double money = 30000;
+    bool is_graduated = true;
+
+    if (age > 17 && money > 25000 || is_graduated)
+    {
+        printf("This is an adult\n");
+    }
+    else
+    {
+        printf("This is not an adult\n");
+    }
     return 0;
 }
