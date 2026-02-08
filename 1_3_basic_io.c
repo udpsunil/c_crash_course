4fd98986078ef56150c7a08ba09a3e435d043e40
Sunil Udupi <udpsunil@gmail.com>
2026-02-08 09:15:08 +0000

    Reading value from console.

--- 1_3_basic_io.c
+++ 1_3_basic_io.c
@@ -5,8 +5,13 @@ int main()
     printf("Hello World\n");
 
     int x = 50; // identifier x is used to represent value of 50
-    int y; //declaration of variable y with a type
-    y = 10; //initialization done with assignment operation.
-    
+    int y;      // declaration of variable y with a type
+    y = 10;     // initialization done with assignment operation.
+
+    printf("%s World has a value of %d\n", "Hello", x);
+
+    int z;
+    scanf("%d", &z); // address of operation & it holds address where the value from scanf should reside.
+    printf("Value of Z is %d\n", z);
     return 0;
 }
